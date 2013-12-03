9f345fc9252f8ac96d91ec85416562c174e3d1c5
Fareed <blueskies_fareed@hotmail.com>
2013-12-03 12:37:00 +0300

    Second Change . add a simple output

--- Test/src/GitTest.java
+++ Test/src/GitTest.java
@@ -3,6 +3,6 @@ public class GitTest {
 	public static void main(String[] args) {
 	    System.out.println("Git is Fun");
 	    System.out.println("Git is Cool");
-	    
+	    System.out.println("Git");
 	}
 }
